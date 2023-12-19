bafaa0b94848ad320b565eb7fee86e81d123d9c8
LSDG0d <104466653+LSDG0d@users.noreply.github.com>
2023-12-20 01:30:13 +0300

    Add files via upload

---
+++ hangman/hangman/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(160, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(160, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ hangman/hangman/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hangman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int health = 12;
+        int words = 220;
+        string word = "";
+        char[] now = new char[10];
+        Random rnd = new Random();
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black, 4);
+            if (health <= 11) g.DrawLine(pen, 20, 240, 20, 220);
+            if (health <= 10) g.DrawLine(pen, 20, 220, 140, 220);
+            if (health <= 9) g.DrawLine(pen, 140, 220, 140, 240);
+            if (health <= 8) g.DrawLine(pen, 60, 40, 60, 220);
+            if (health <= 7) g.DrawLine(pen, 60, 40, 120, 40);
+            if (health <= 6) g.DrawLine(pen, 120, 40, 120, 60);
+            if (health <= 5) g.DrawEllipse(pen, 100, 60, 40, 40);
+            if (health <= 4) g.DrawLine(pen, 120, 100, 120, 160);
+            if (health <= 3) g.DrawLine(pen, 120, 100, 140, 140);
+            if (health <= 2) g.DrawLine(pen, 120, 100, 100, 140);
+            if (health <= 1) g.DrawLine(pen, 120, 158, 130, 210);
+            if (health <= 0) g.DrawLine(pen, 119, 158, 110, 210);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Invalidate();
+            StreamReader sr = new StreamReader("List.txt");
+            int choice = rnd.Next(0, words);
+            while (choice >= 0)
+            {
+                word = sr.ReadLine();
+                choice--;
+            }
+            label1.Text = "";
+            label2.Text = "";
+            for (int i = 0; i < word.Length; i++) now[i] = '*';
+            for (int i = 0; i < word.Length; i++) label1.Text += now[i];
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char p = e.KeyChar;
+            bool b = true;
+            for (int i = 0; i < word.Length; i++) if (p == word[i] && now[i] == '*')
+                {
+                    now[i] = p;
+                    b = false;
+                }
+            if (b) health--;
+            label1.Text = "";
+            label2.Text += p;
+            for (int i = 0; i < word.Length; i++) label1.Text += now[i];
+            this.Invalidate();
+            if (health <= 0)
+            {
+                MessageBox.Show("Game Over!!! It was " + word.ToString());
+                Application.Exit();
+            }
+            if (label1.Text == word)
+            {
+                MessageBox.Show("You win!!!");
+                Application.Exit();
+            }
+        }
+    }
+}
